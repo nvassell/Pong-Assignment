f6d96bcd172bd172bd971f7b9a37075b60d0c53f
Nathan Vassell <36028839+nvassell@users.noreply.github.com>
2021-01-19 03:25:02 -0800

    Base game
    base game, no score or anything, just gameplay.

---
+++ Pong/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Ball")]
+    public GameObject ball;
+
+    [Header("Player 1")]
+    public GameObject player1Paddle;
+    public GameObject player1Goal;
+
+    [Header("Player 2")]
+    public GameObject player2Paddle;
+    public GameObject player2Goal;
+
+    public void Player1Scored()
+    {
+        ResetPosition();
+    }
+
+    public void Player2Scored()
+    {
+        ResetPosition();
+    }
+
+    public void ResetPosition()
+    {
+        ball.GetComponent<ball>().Reset();
+        player1Paddle.GetComponent<paddle>().Reset();
+        player2Paddle.GetComponent<paddle>().Reset();
+    }
+}
