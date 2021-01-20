2e72d5cf19a615ab6f7fd70cab8d8f8a9bea4c88
Nathan Vassell <36028839+nvassell@users.noreply.github.com>
2021-01-19 17:10:59 -0800

    Pause menu added
    Added pause menu
     - Resume button added
     - Player controls added
     - Exit button added
    
     - "ESC" to close game removed

--- Pong/Assets/GameManager.cs
+++ Pong/Assets/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -15,13 +16,24 @@ public class GameManager : MonoBehaviour
     public GameObject player2Paddle;
     public GameObject player2Goal;
 
+    [Header("Score UI")]
+    public GameObject player1Text;
+    public GameObject player2Text;
+
+    private int player1Score;
+    private int player2Score;
+
     public void Player1Scored()
     {
+        player1Score++;
+        player1Text.GetComponent<TextMeshProUGUI>().text = player1Score.ToString();
         ResetPosition();
     }
 
     public void Player2Scored()
     {
+        player2Score++;
+        player2Text.GetComponent<TextMeshProUGUI>().text = player2Score.ToString();
         ResetPosition();
     }
 
@@ -32,12 +44,5 @@ public class GameManager : MonoBehaviour
         player2Paddle.GetComponent<paddle>().Reset();
     }
 
-    void Update()
-    {
-        if (Input.GetKey("escape"))
-        {
-            Application.Quit();
-        }
-    }
 
 }
